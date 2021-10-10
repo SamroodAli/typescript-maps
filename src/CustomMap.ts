e0f79f2d7b84b6156c293ac2d355f040c8deccf0
samroodAli <devsamrood@gmail.com>
2021-10-10 14:00:20 +0530

    Refactor: Refactor addMarker function using interface Mappable

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,5 +1,9 @@
-import User from "./User";
-import Company from "./Company";
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
 
 export default class CustomMap {
   private googleMap: google.maps.Map;
@@ -9,23 +13,13 @@ export default class CustomMap {
       center: { lat: 0, lng: 0 },
     });
   }
-
-  addUserMarker = (user: User): void => {
+  addMarker(mappable: Mappable) {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng,
+        lat: mappable.location.lat,
+        lng: mappable.location.lat,
       },
     });
-  };
-  addCompanyMarker = (company: Company): void => {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng,
-      },
-    });
-  };
+  }
 }
